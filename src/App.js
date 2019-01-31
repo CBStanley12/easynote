1fbb30133e450509acf8716cb0b242acdf4edbe3
CBStanley12 <cbstanley12@gmail.com>
2019-01-30 21:51:38 -0700

    Added components to main App.js file

--- src/App.js
+++ src/App.js
@@ -1,37 +1,86 @@
 import React, { Component } from 'react';
 import './App.css';
+const Editor = require('./components/Editor');
+const Preview = require('./components/Preview');
+const Toolbar = require('./components/Toolbar');
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markdown: placeholder,
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      markdown: e.target.value
+    });
+  }
+  
   render() {
     return (
-      <div className="container">
-        <div id="app" class="row">
-          <Editor />
-          <Preview />
+      <div className="row m-1 p-3">
+        <div className="col-sm-12 col-md-5 p-2 border">
+          <Toolbar icon="fa fa-pencil-square-o" text="  Editor" />
+          <Editor markdown={this.state.markdown} onChange={this.handleChange} />
+        </div>
+        <div className="col-sm-12 col-md-6 ml-auto p-2 border">
+          <Toolbar icon="fa fa-file-text-0" text="  Preview" />
+          <Preview markdown={this.state.markdown} />
         </div>
       </div>
     );
   }
 }
 
-class Editor extends Component {
-  render() {
-    return (
-      <div className="col-sm-12 col-6">
-        <textarea id="editor" />
-      </div>
-    );
-  }
-}
+const placeholder =
+`# Welcome to my React Markdown Previewer!
 
-class Preview extends Component {
-  render() {
-    return (
-      <div className="col-sm-12 col-6">
-        
-      </div>
-    );
+## This is a sub-heading...
+### And here's some other cool stuff:
+  
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
   }
 }
+\`\`\`
+  
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | ------------- 
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want! 
+1. But the list goes on...
+- Even if you use dashes or asterisks.
+* And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://goo.gl/Umyytc)
+`
 
 export default App;
