61af76baddc7a25f669541619776d26c41c2b214
CBStanley12 <cbstanley12@gmail.com>
2019-01-30 21:14:34 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <div id="app" class="row">
+          <Editor />
+          <Preview />
+        </div>
+      </div>
+    );
+  }
+}
+
+class Editor extends Component {
+  render() {
+    return (
+      <div className="col-sm-12 col-6">
+        <textarea id="editor" />
+      </div>
+    );
+  }
+}
+
+class Preview extends Component {
+  render() {
+    return (
+      <div className="col-sm-12 col-6">
+        
       </div>
     );
   }
