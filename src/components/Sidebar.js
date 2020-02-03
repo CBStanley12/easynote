b1eb7745ca296b9dce6d10887dec4693208ba3a3
Christopher Stanley <chris@cbstanley.dev>
2020-02-03 00:22:05 -0700

    add aria-labels and tooltips to icon buttons

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -14,10 +14,10 @@ const Sidebar = ({ notes, activeNote, selectNote, newNote }) => {
     return (
         <aside className="layout-sidebar sidebar" data-active="is-active">
             <header className="sidebar_header">
-                <button className="icon">
+                <button className="icon icon-left" aria-label="Menu">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
                 </button>
-                <button className="icon" onClick={newNote}>
+                <button className="icon icon-left" onClick={newNote} aria-label="New Note">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg>
                 </button>
             </header>
