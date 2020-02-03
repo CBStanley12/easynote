a69f119f677b70c367652cd524a9da35684f1f3c
Christopher Stanley <chris@cbstanley.dev>
2020-02-02 22:11:40 -0700

    add ability to create notes and adjust border colors

--- src/components/App.js
+++ src/components/App.js
@@ -32,14 +32,45 @@ class App extends Component {
       activeNote: 0,
       isPreviewDisplayed: false
     }
+    this.createNewNote = this.createNewNote.bind(this);
     this.handleNoteEdit = this.handleNoteEdit.bind(this);
     this.togglePreview = this.togglePreview.bind(this);
     this.selectNote = this.selectNote.bind(this);
   }
 
+  /*
+    TODO: Function to toggle the sidebar
+    TODO: Function to create a new note
+    TODO: Function to delete a note
+    TODO: Functions to retrieve/store notes in localStorage
+  */
+
+
+  // Function to create a new note
+  createNewNote() {
+    let notesCopy = this.state.notes;
+    let lastID = notesCopy[notesCopy.length - 1].id;
+
+    let newNote = {
+      id: (lastID + 1),
+      title: "New Note...",
+      preview: "",
+      text: ""
+    };
+
+    notesCopy.push(newNote);
+
+    this.setState({
+      notes: notesCopy,
+      activeNote: (notesCopy.length - 1)
+    });
+
+    document.querySelector('textarea').focus();
+  }
+
   // Function to handle editing of the current note
   handleNoteEdit(e) {
-    let newNotes = this.state.notes;
+    let notesCopy = this.state.notes;
     let index = this.state.activeNote;
     const regex = /#{1,}|\*{1,}/gm;
 
@@ -47,12 +78,12 @@ class App extends Component {
     let newTitle = e.target.value.split('\n', 1)[0].replace(regex, '');
     let newPreview = e.target.value.substring((newTitle.length + 1)).replace(regex, '');
 
-    newNotes[index].text = newText;
-    newNotes[index].title = newTitle;
-    newNotes[index].preview = newPreview;
+    notesCopy[index].text = newText;
+    notesCopy[index].title = e.target.value ? newTitle : "New Note...";
+    notesCopy[index].preview = newPreview;
 
     this.setState({
-      notes: newNotes
+      notes: notesCopy
     });
   }
 
@@ -74,7 +105,7 @@ class App extends Component {
 
   render() {
     const { notes, activeNote, isPreviewDisplayed } = this.state;
-    const { handleNoteEdit, togglePreview, selectNote } = this;
+    const { handleNoteEdit, togglePreview, selectNote, createNewNote } = this;
 
     let content;
 
@@ -87,7 +118,7 @@ class App extends Component {
     return (
       <div className="layout-container">
         <Header click={togglePreview} isPreviewDisplayed={isPreviewDisplayed} />
-        <Sidebar notes={notes} activeNote={activeNote} click={selectNote} />
+        <Sidebar notes={notes} activeNote={activeNote} click={selectNote} newNote={createNewNote} />
         {content}
       </div>
     );
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Sidebar = ({ notes, activeNote, click }) => {
+const Sidebar = ({ notes, activeNote, click, newNote }) => {
     return (
         <aside className="layout-sidebar sidebar">
             <header className="sidebar_header">
                 <button className="icon">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
                 </button>
-                <button className="icon">
+                <button className="icon" onClick={newNote}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg>
                 </button>
             </header>
