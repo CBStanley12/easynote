3a63b6a6b1a7527c4d03d514157d4526c38e2ac2
Christopher Stanley <cbstanley12@gmail.com>
2020-02-11 19:43:45 -0700

    fix menu text size and spacing on mobile

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -74,17 +74,17 @@ const Menu = ({ isMenuDisplayed, theme, font, changeTheme, changeFont }) => {
                     <svg className="menu-icon menu-icon--title" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="16" x2="12" y2="12"></line><line x1="12" y1="8" x2="12.01" y2="8"></line></svg>
                     Information
                 </h4>
-                <a className="menu-link" aria-label="External Link" href="https://github.com/CBStanley12/markdown-previewer" target="_blank" rel="noopener noreferrer">
+                <a className="menu_section-link" aria-label="External Link" href="https://github.com/CBStanley12/markdown-previewer" target="_blank" rel="noopener noreferrer">
                     <svg className="menu-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
                     View Project on GitHub
                 </a>
-                <a className="menu-link" aria-label="External Link" href="https://github.com/CBStanley12/markdown-previewer/issues/new" target="_blank" rel="noopener noreferrer">
+                <a className="menu_section-link" aria-label="External Link" href="https://github.com/CBStanley12/markdown-previewer/issues/new" target="_blank" rel="noopener noreferrer">
                     <svg className="menu-icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"></path><line x1="12" y1="9" x2="12" y2="13"></line><line x1="12" y1="17" x2="12.01" y2="17"></line></svg>
                     Report an Issue
                 </a>
             </div>
             <footer className="menu_section menu_section--center">
-                <small className="menu_section-copyright">&copy; 2019 <a className="menu-link" aria-label="External Link" href="https://cbstanley.dev" target="_blank" rel="noopener noreferrer">Christopher Stanley</a></small>
+                <small className="menu_section-copyright"><a className="menu_section-copyright" href="https://cbstanley.dev" target="_blank" rel="noopener noreferrer">Coded by Christopher Stanley</a></small>
             </footer>
         </div>
     )
