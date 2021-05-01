054f30726bd04c36c1aa7d75090b42b64bfa1ea7
Christopher Stanley <chris@cbstanley.dev>
2021-05-01 17:42:26 -0600

    refactor: add svg files for button icons

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,9 +1,19 @@
 import './styles.css';
 import React, { useState, useEffect } from 'react';
 import {ReactComponent as CreateIcon} from '../SVG/create.svg';
+import {ReactComponent as SidebarHideIcon} from '../SVG/sidebar-hide.svg';
+import {ReactComponent as SidebarShowIcon} from '../SVG/sidebar-show.svg';
+import {ReactComponent as MarkdownHideIcon} from '../SVG/markdown-hide.svg';
+import {ReactComponent as MarkdownShowIcon} from '../SVG/markdown-show.svg';
+import {ReactComponent as DeleteIcon} from '../SVG/delete.svg';
 
 const ICONS = {
 	create: <CreateIcon />,
+	sidebarHide: <SidebarHideIcon />,
+	sidebarShow: <SidebarShowIcon />,
+	markdownHide: <MarkdownHideIcon />,
+	markdownShow: <MarkdownShowIcon />,
+	delete: <DeleteIcon />,
 };
 
 const Button = ({ cls, click, label, icon }) => {
